599a349f0c2ab45b9c67982b55983e4d4b009d2d
Den <90329284+zadumka@users.noreply.github.com>
2024-01-08 17:23:36 +0200

    delete friends file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 // import React from 'react';
-import Profile from "./components/Profile/Profile";
-import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
-import userData from "./userData.json";
-import transactions from "./transactions.json";
+import Profile from './components/Profile/Profile';
+import FriendList from './components/FriendList/FriendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+import userData from './userData.json';
+import friends from './friends.json';
+import transactions from './transactions.json';
 
 const App = () => {
   return (
